bdb32a48ad66f39c602dfd69f710a9af6dfa92c1
999Ralex999 <140698368+999Ralex999@users.noreply.github.com>
2024-02-06 16:22:40 +0200

    first part done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+function setupColorChanger() {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  let colorChangeInterval = null;
+
+  startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    colorChangeInterval = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
+
+  stopButton.addEventListener('click', () => {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(colorChangeInterval);
+  });
+
+  stopButton.disabled = true;
+}
+
+document.addEventListener('DOMContentLoaded', setupColorChanger);
